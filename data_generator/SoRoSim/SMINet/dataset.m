f10985644313c192dce397cf83949cbef259fa78
l5vd5 <l5vd5@korea.ac.kr>
2022-02-08 10:08:37 +0900

    feat: density

--- data_generator/SoRoSim/SMINet/dataset.m
+++ data_generator/SoRoSim/SMINet/dataset.m
@@ -21,18 +21,24 @@ a = NodlgStatics(S,zeros(S.ndof,1),[20,0,0]);
 S.plotq0()
 
 %%
-xyz = zeros(1000,33);
-l = 0;
-for i = 7:8:80
-    for j = 1:8:80
-        for k = 1:8:80
-            a = NodlgStatics(S,zeros(S.ndof,1),[-i, -j, -k]);
-            centers = plotqGetPosition(S,a,'FaceAlpha',0.1);
-            l = l+1;
-            xyz(l,:) = [i,j,k,centers*100];
+for ii = 1:2:8
+    for jj = 1:2:8
+        for kk = 1:2:8
+            xyz = zeros(1000,33);
+            l = 0;
+            for i = ii:8:80
+                for j = jj:8:80
+                    for k = kk:8:80
+                        a = NodlgStatics(S,zeros(S.ndof,1),[-i, -j, -k]);
+                        centers = plotqGetPosition(S,a,'FaceAlpha',0.1);
+                        l = l+1;
+                        xyz(l,:) = [i,j,k,centers*100];
+                    end
+                end
+            end
+            writematrix(xyz, ['data',num2str(ii),num2str(jj),num2str(kk),'.txt'])
         end
     end
 end
 %% Save
 
-writematrix(xyz, 'data711.txt')
